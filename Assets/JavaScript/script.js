6c67b81c8c66697223fe80da127e36c3223f75f5
Christian Mendez <christianmendez@Christians-MBP.attlocal.net>
2022-11-17 02:48:07 -0600

    files for quiz

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,31 @@
+
+
+//QUESTIONS ARRAY 
+
+var quizQuestions = [
+    {
+    title: "Commonly used data types DO Not Include",
+    options: ['string', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts'
+    },
+    {
+    title: "The condition in an if / else statement is enclosed with____",
+    options: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
+    answer: 'parenthesis'
+    },
+    {
+    title: "Arrays in JavaScript can be used to store",
+    options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+    answer: 'all of the above'
+    },
+    {
+    title: "String values must be enclosed within____ when being assigned to variables",
+    options: ['commas', 'curley brackets', 'quotes', 'parenthesis'],
+    answer: 'quotes'
+    },
+    {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: ['JavaScript', 'terminal/bash', 'for loop', 'console.log()'],
+    answer: 'console.log()'
+    },
+]
